58775dfb70f77d1a39e1adc0d335ba4e17a0efcb
rsk <3238715413@qq.com>
2022-05-06 18:54:31 +0800

    master maven-git-test commit3

--- src/main/java/com/atguigu/git/Gittest.java
+++ src/main/java/com/atguigu/git/Gittest.java
@@ -8,5 +8,6 @@ public class Gittest {
         System.out.println("hello git4");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("hot-fix test2");
     }
 }
